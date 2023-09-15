e9e6f63c95c5465f4c8c7ce15b62d62673f1cdfd
madarsh211 <117467368+madarsh211@users.noreply.github.com>
2023-09-16 00:58:06 +0530

    Add files via upload
    performed CRUD operations on an E-commerce webapp, using MVC ,N-Tier Architecture Applied

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿
+using Bulky.Models;
+using Bulkyweb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAcess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<category>().HasData(
+                new category { Id = 1, Name = "Action", DisplayOrder=1 },
+                new category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new category { Id = 3, Name = "History", DisplayOrder = 3 }
+               );
+        }
+    }
+}
---
+++ Bulky.DataAccess/Migrations/20230728152904_SeedCategoryTable.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAcess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Displayorder",
+                table: "categories",
+                newName: "DisplayOrder");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DisplayOrder",
+                table: "categories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 2, "SciFi" },
+                    { 3, 3, "History" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "DisplayOrder",
+                table: "categories",
+                newName: "Displayorder");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Displayorder",
+                table: "categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
